2287a9843696b690ae449eae0e267dafe53c90f8
aleksandrtk <57760531+aleksandrtk@users.noreply.github.com>
2025-04-11 12:32:53 +0300

    Update main.tf

--- k8s/main.tf
+++ k8s/main.tf
@@ -6,20 +6,39 @@ provider "helm" {
   }
 }
 
+# Проверка, существует ли релиз jenkins
+data "helm_release" "jenkins_check" {
+  name      = "jenkins"
+  namespace = "jenkins"
+  # Если релиз не существует, то вернется ошибка и count будет равен 1
+  # Можно добавить depends_on для предотвращения взаимозависимостей
+}
+
+# Создание релиза Jenkins только если его нет
 resource "helm_release" "jenkins" {
+  count            = length(data.helm_release.jenkins_check.*.name) == 0 ? 1 : 0
   name             = "jenkins"
   chart            = "jenkins"
   repository       = "https://charts.jenkins.io"
   namespace        = "jenkins"
   create_namespace = true
-  replace = false
+
+  # Параметры для управления релизом
+}
+
+# Аналогично для argocd
+data "helm_release" "argocd_check" {
+  name      = "argocd"
+  namespace = "argocd"
 }
 
 resource "helm_release" "argocd" {
+  count            = length(data.helm_release.argocd_check.*.name) == 0 ? 1 : 0
   name             = "argocd"
   chart            = "argo-cd"
   repository       = "https://argoproj.github.io/argo-helm"
   namespace        = "argocd"
   create_namespace = true
-  replace = false
+
+  # Параметры для управления релизом
 }
