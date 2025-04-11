e782ef59956b3b0c0d816d9ca881947a8f162ce9
aleksandrtk <57760531+aleksandrtk@users.noreply.github.com>
2025-04-11 11:09:10 +0300

    Create main.tf

---
+++ k8s/main.tf
@@ -0,0 +1,22 @@
+provider "helm" {
+  kubernetes {
+    config_path = "~/.kube/config"
+  }
+}
+
+resource "helm_release" "jenkins" {
+  name       = "jenkins"
+  chart      = "jenkins"
+  repository = "https://charts.jenkins.io"
+  namespace  = "jenkins"
+  create_namespace = true
+}
+
+resource "helm_release" "argocd" {
+  name       = "argocd"
+  chart      = "argo-cd"
+  repository = "https://argoproj.github.io/argo-helm"
+  namespace  = "argocd"
+  create_namespace = true
+#  values = [file("terraform/argocd-values.yaml")]
+}
